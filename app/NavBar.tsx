1a81ad5f7173440e9fb090f1199f0e9600bfef43
ainhan-fullstack <nhanphamit96@gmail.com>
2025-09-01 21:02:43 +0930

    Set up Prisma for the databse

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,25 +1,36 @@
-import Link from 'next/link'
-import React from 'react'
-import { AiFillBug } from 'react-icons/ai'
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
-
-    const links = [
-        {lable: 'Dashboard', href: '/'},
-        {lable: 'Issues', href: '/issues'}
-    ]
+  const currentPath = usePathname();
+  const links = [
+    { lable: "Dashboard", href: "/" },
+    { lable: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><AiFillBug/></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => 
-                <Link key={'Navbar_' + link.lable} 
-                className='text-zinc-500 hover:text-zinc-800 transition-colors' 
-                href={link.href}>{link.lable}</Link>)
-            }
-        </ul>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={"Navbar_" + link.lable}
+            className={`${
+              link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+            } text-zinc-500 hover:text-zinc-800 transition-colors`}
+            href={link.href}
+          >
+            {link.lable}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
